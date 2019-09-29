5ea1bde832d771cbcf6c71c6b22a769bc4798ec9
admin <admin@admins-MacBook-Pro.local>
2019-09-28 17:24:22 -0700

    my display result is working and i have to work to reset function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,147 +1,169 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-var quizQuestion = [
-    {
-        question: "who is the first president of USA?",
-        choices: ["George Washington", "James Madison", "john quincy Adams", "Thomas Jefferson"],
-        correctAnswer: "George Washington"
-    },
-    {
-        question: "what is the Capital city of Japan?",
-        choices: ["Miyazaki", "Tokyo", "Nagoya", "kyoto"],
-        correctAnswer: "Tokyo"
-    },
-    {
-        question: "what is the Capital city of Japan?",
-        choices: ["Miyazaki", "tokyo", "Nagoya", "kyoto"],
-        correctAnswer: "tokyo"
-    },
-    {
-        question: "where is the tallest Mountain in the world?",
-        choices: ["Japan", "Rasia", "India", "Nepal"],
-        correctAnswer: "Nepal"
-    },
-    {
-        question: "The most popular fruit in the world?",
-        choices: ["Apple", "Banana", "Tomato", "Orange"],
-        correctAnswer: "Tomato"
-    },
-    {
-        question: "what is world biggest animal?",
-        choices: ["Blue whale", "Ostrich", "Saltwater Crocodile", "Whale shark"],
-        correctAnswer: "Blue whale"
-    },
-    {
-        question: "which is the largest state in US by area?",
-        choices: ["Texas", "Virginia", "New york", "Alaska"],
-        correctAnswer: "Alaska"
-    },
-    {
-        question: "what is the annual inflation rate in united state?",
-        choices: ["1.7%", "1.8%", "2%", "0.5%"],
-        correctAnswer: "1.7%"
-    },
-    {
-        question: "when was the first iphone introduced?",
-        choices: ["June 29, 2007", "June 25, 2007", "july 01, 2008", "march 09, 2006"],
-        correctAnswer: "June 29, 2007"
-    },
-    {
-        question: "when was the space needle built?",
-        choices: ["April 20, 1961", "April 10, 1962", "march 17, 1959", "April 17, 1961"],
-        correctAnswer: "April 17, 1961"
+    var quizQuestion = [
+        {
+            question: "who is the first president of USA?",
+            choices: ["George Washington", "James Madison", "john quincy Adams", "Thomas Jefferson"],
+            correctAnswer: "George Washington"
+        },
+        {
+            question: "what is the Capital city of Japan?",
+            choices: ["Miyazaki", "Tokyo", "Nagoya", "kyoto"],
+            correctAnswer: "Tokyo"
+        },
+        {
+            question: "what is the Capital city of Japan?",
+            choices: ["Miyazaki", "tokyo", "Nagoya", "kyoto"],
+            correctAnswer: "tokyo"
+        },
+        {
+            question: "where is the tallest Mountain in the world?",
+            choices: ["Japan", "Rasia", "India", "Nepal"],
+            correctAnswer: "Nepal"
+        },
+        {
+            question: "The most popular fruit in the world?",
+            choices: ["Apple", "Banana", "Tomato", "Orange"],
+            correctAnswer: "Tomato"
+        },
+        {
+            question: "what is world biggest animal?",
+            choices: ["Blue whale", "Ostrich", "Saltwater Crocodile", "Whale shark"],
+            correctAnswer: "Blue whale"
+        },
+        {
+            question: "which is the largest state in US by area?",
+            choices: ["Texas", "Virginia", "New york", "Alaska"],
+            correctAnswer: "Alaska"
+        },
+        {
+            question: "what is the annual inflation rate in united state?",
+            choices: ["1.7%", "1.8%", "2%", "0.5%"],
+            correctAnswer: "1.7%"
+        },
+        {
+            question: "when was the first iphone introduced?",
+            choices: ["June 29, 2007", "June 25, 2007", "july 01, 2008", "march 09, 2006"],
+            correctAnswer: "June 29, 2007"
+        },
+        {
+            question: "when was the space needle built?",
+            choices: ["April 20, 1961", "April 10, 1962", "march 17, 1959", "April 17, 1961"],
+            correctAnswer: "April 17, 1961"
+        }
+    ]
+    // console.log(quizQuestion);
+    //initail values//
+    var counter = 30;
+    var currentQuestion = 0;
+    var score = 0;
+    var lost = 0;
+    var timer;
+    var result = 0;
+
+    // if the timer is over, then go to the next question//
+    function nextQuestion() {
+        console.log("nextquestion currentQuestion: ", currentQuestion);
+        console.log("nextquestion quizQuestion.length - 1: ", quizQuestion.length - 1);
+        var isQuestionOver = (quizQuestion.length - 1) === currentQuestion;
+        if (isQuestionOver) {
+            console.log("The game is over");
+            Displayresult();
+        } else {
+            currentQuestion++;
+            loadQustion();
+        }
     }
-]
-// console.log(quizQuestion);
-//initail values//
-var counter = 30;
-var currentQuestion = 0;
-var score = 0;
-var lost = 0;
-var timer;
-
-// if the timer is over, then go to the next question//
-function nextQuestion() {
-    var isQuestionOver = (quizQuestion.length - 1) === currentQuestion;
-    if (isQuestionOver) {
-        console.log("The game is over");
-    }
-    currentQuestion++;
-    loadQustion();
-}
-// start a 30 second timer for user to respond or choose an answer to each question//
-function timeUp() {
-    clearInterval(timer);
-    lost++;
-    nextQuestion();
-}
-
-function countDown() {
-    counter--;
-    $("#timer").html("timer;" + counter)
-    if (counter === 0) {
-        timeUp();
-    }
-}
-// console.log($);
-// Display the quetions and the choices to the browser//
-function loadQustion() {
-    counter = 5;
-    timer = setInterval(countDown, 1000);
-    const question = quizQuestion[currentQuestion].question;
-    const choices = quizQuestion[currentQuestion].choices;
-    $("#timer").html("timer:" + counter);
-    $("#Game").html("<h4>" + question + "</h4>");
-    var $choicesElements = $(loadChoices(choices));
-    $("#Game").append($choicesElements);
-
-    // console.log(loadQustion);
-}
-
-function loadChoices(choices) {
-    var result = " ";
-    for (var i = 0; i < choices.length; i++) {
-        result += '<p class ="choice" data-answer="' + choices[i] + '">' + choices[i] + '</p>';
-
-    }
-    return result;
-}
-$(document).on("click", ".choice", function () {
-    clearInterval(timer);
-    var selectAnswer = $(this).attr("data-answer");
-    var correctAnswer = quizQuestion[currentQuestion].correctAnswer;
-    console.log(correctAnswer);
-    if (correctAnswer === selectAnswer) {
-        score++;
-        console.log("winss!!!");
-        nextQuestion();
-    }
-    else {
-        lost++
-        console.log("you lost");
+    // start a 30 second timer for user to respond or choose an answer to each question//
+    function timeUp() {
+        clearInterval(timer);
+        lost++;
         nextQuestion();
     }
 
-    console.log("yapp");
-});
+    function countDown() {
+        counter--;
+        $("#Timer").html("Timer:" + counter)
+        if (counter === 0) {
+            timeUp();
+        }
+    }
+    // console.log($);
+    // Display the quetions and the choices to the browser//
+    function loadQustion() {
+        counter = 5;
+        timer = setInterval(countDown, 1000);
+        const question = quizQuestion[currentQuestion].question;
+        const choices = quizQuestion[currentQuestion].choices;
+        $("#Timer").html("Timer:" + counter);
+        $("#Game").html("<h4>" + question + "</h4>");
+        var $choicesElements = $(loadChoices(choices));
+        $("#Game").append($choicesElements);
 
-function Display() {
-    var result = 
+        // console.log(loadQustion);
+    }
+
+    function loadChoices(choices) {
+        var result = " ";
+        for (var i = 0; i < choices.length; i++) {
+            result += '<p class ="choice" data-answer="' + choices[i] + '">' + choices[i] + '</p>';
+
+        }
+        return result;
+    }
+    $(document).on("click", ".choice", function () {
+        clearInterval(timer);
+        var selectAnswer = $(this).attr("data-answer");
+        var correctAnswer = quizQuestion[currentQuestion].correctAnswer;
+        console.log(correctAnswer);
+        if (correctAnswer === selectAnswer) {
+            score++;
+            console.log("winss!!!");
+            nextQuestion();
+        }
+        else {
+            lost++
+            console.log("you lost");
+            nextQuestion();
+        }
+
+        console.log("yapp");
+    });
+
+    function Displayresult() {
+        // $("#total-right").html("total-right:", score);
+        // $("#total-wrong").html("total-wrong:", lost)
+        // $("#total").html("total:", question.length)
+
+        var result = '<p> you get: '+ score + ' questions(s)right</p>' + 
+      '<p> you missed: '+ lost + ' questions(s)right</p>' +
+       '<p>total questions: ' + quizQuestion.length + ' questions(s)right</p>' +
+       '<button>Reset Game + </button>'
+        $("#Game").html(result);
+
+    }
+
+
         $("#game").html(result);
-}
 
 
-loadQustion();
 
-//loadChoices();
+        loadQustion();
 
-$(".button").on("click", function() {
-    console.log("button is working");
-    $(".button").remove();
-    $("#timer").html(counter);
-    // loadQustion();
-})
+        //loadChoices();
 
-});
+        $(".button").on("click", function () {
+            console.log("button is working");
+            $(".button").remove();
+            $("#Timer").html(counter);
+            // loadQustion();
+        })
+
+    });
+    function reset (){
+        Timer = 0;
+        loadQustion();
+    }
+// $(document).on("click", ".choice", function () {
